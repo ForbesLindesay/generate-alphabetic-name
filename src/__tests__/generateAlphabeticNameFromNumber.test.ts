800a5f47ad534fbd2ad57671256c1d0c5fb00f20
Forbes Lindesay <forbes@lindesay.co.uk>
2022-01-18 10:33:01 +0000

    fix: duplicate IDs were more likely than they should have been
    `generateAlphabeticNameFromNumber` could sometimes generate collisions.
    
    BREAKING CHANGE: some hashes will have changed.

--- src/__tests__/generateAlphabeticNameFromNumber.test.ts
+++ src/__tests__/generateAlphabeticNameFromNumber.test.ts
@@ -8,3 +8,20 @@ test('creates alphabetic names for number input data', () => {
 test('does not fail for numbers above int32 limit', () => {
   expect(generateAlphabeticNameFromNumber(3819806601)).toEqual('kcwstn');
 });
+
+test('does not generate duplicates in at least the first 1,000,000 integers', () => {
+  const seen = new Set();
+  for (let i = 0; i < 1_000_000; i++) {
+    const name = generateAlphabeticNameFromNumber(i);
+    if (seen.has(name)) {
+      throw new Error(`Found duplicate id at ${i}: ${name}`);
+    }
+    seen.add(name);
+  }
+});
+
+test('even the largest possible hash is still a short string', () => {
+  expect(generateAlphabeticNameFromNumber(Math.pow(2, 32) - 1)).toEqual(
+    `lpvJyV`,
+  );
+});
